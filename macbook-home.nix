8bb35bc860ba93eb8dd6c9c6028f2e3a354f248c
Natsu Kagami <nki@nkagami.me>
2021-10-19 23:00:44 -0400

    Remove command-not-found

--- macbook-home.nix
+++ macbook-home.nix
@@ -24,7 +24,6 @@ in
       brew64 = "arch -x86_64 /usr/local/bin/brew";
   };
 
-
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
