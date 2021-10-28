52d0f60f195c7bef6f17e358d3e04c71090eb432
Natsu Kagami <nki@nkagami.me>
2021-10-28 16:07:49 -0400

    Basic module set up

---
+++ modules/my-tinc/hosts.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:
+
+with lib;
+let
+  hosts = import ./hosts;
+
+  cfg = config.services.my-tinc;
+
+  mapAttrs = f: attrs: builtins.listToAttrs (
+    map (name: { inherit name; value = f name (builtins.getAttr name attrs); }) (builtins.attrNames attrs)
+  );
+in
+{
+  config = mkIf cfg.enable {
+    # All hosts we know of
+    services.tinc.networks.my-tinc.hostSettings = mapAttrs (name: host: {
+      addresses = [ { inherit (host) address; } ];
+      subnets = [ { address = host.subnetAddr; } ];
+      rsaPublicKey = mkIf (host ? "rsaPublicKey") (builtins.readFile host.rsaPublicKey);
+      ed25519PublicKey = mkIf (host ? "ed25519PublicKey") (builtins.readFile host.ed25519PublicKey);
+    }) hosts;
+  };
+}
---
+++ modules/my-tinc/hosts/default.nix
@@ -0,0 +1,10 @@
+{
+  # TODO: Edit the list of hosts here.
+  hosts = {
+    nki-cloud = {
+      subnetAddr = "10.0.0.10";
+      address = "nki.personal";
+      rsaPublicKey = ./nki-cloud;
+    };
+  };
+}
