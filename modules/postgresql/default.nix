6efb74062bbcc68b28f7aad6bf861613899316ef
Natsu Kagami <nki@nkagami.me>
2021-10-31 16:33:27 -0400

    Set up PostgreSQL

---
+++ modules/postgresql/default.nix
@@ -0,0 +1,42 @@
+{ pkgs, config, lib, ... } :
+
+with lib;
+let
+  cfg = config.cloud.postgresql;
+
+  # From a database name, create an "ensureUser"
+  # entry with the same name and assign all permissions
+  # to that database.
+  userFromDatabase = databaseName : {
+    name = databaseName;
+    ensurePermissions = {
+      "DATABASE ${databaseName}" = "ALL PRIVILEGES";
+    };
+  };
+in
+{
+  options.cloud.postgresql.databases = mkOption {
+    type = types.listOf types.str;
+    default = [];
+    description = ''
+      The list of databases to be created.
+      An user with the same name
+      and full access to the database will be created.
+    '';
+  };
+
+  # PostgreSQL settings.
+  config.services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_13;
+
+    ensureDatabases = cfg.databases;
+
+    ensureUsers = map userFromDatabase cfg.databases;
+  };
+
+  # Backup settings
+  config.services.postgresqlBackup = {
+    enable = true;
+  };
+}
