fd0c28e3e196cf817f01b613dac4ffc0a4c98dca
Natsu Kagami <nki@nkagami.me>
2021-11-03 13:22:27 -0400

    Try adding youmubot

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
     rnix-lsp.url = "github:nix-community/rnix-lsp";
     rnix-lsp.inputs.naersk.follows = "naersk";
     rnix-lsp.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    youmubot.url = "github:natsukagami/youmubot/flake-module";
   };
 
   outputs = { self, darwin, nixpkgs, nixpkgs-unstable, home-manager-unstable, home-manager-21_05, deploy-rs, sops-nix, nur, ... }@inputs:
@@ -62,6 +63,7 @@
         system = "x86_64-linux";
         modules = [
           ./modules/my-tinc
+          inputs.youmubot.nixosModule.x86_64-linux
           sops-nix.nixosModules.sops
           ./nki-personal-do/configuration.nix
           (overlayForSystem "x86_64-linux")
