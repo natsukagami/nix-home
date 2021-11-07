8cc7f126d5751c2af2eae4132f278cfafa5381f5
Natsu Kagami <nki@nkagami.me>
2021-11-07 13:51:14 -0500

    Add ibus configuration for X11

--- home/X11/default.nix
+++ home/X11/default.nix
@@ -1,47 +1,57 @@
-{ pkgs, config, lib, ... } :
+{ pkgs, config, lib, ... }:
 
+with lib;
 {
-    imports = [ ./packages.nix ];
+  imports = [ ./packages.nix ];
 
-    home.sessionVariables = {
-        # Set up Java font style
-        _JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=lcd";
-    };
+  home.sessionVariables = {
+    # Set up Java font style
+    _JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=lcd";
+  };
 
-    # X Session settings
-    xsession.enable = true;
+  # X Session settings
+  xsession.enable = true;
 
-    # Wallpaper
-    home.file.wallpaper = {
-        source = ./. + "/wallpaper.jpg";
-        target = "wallpaper.jpg";
-    };
+  # Wallpaper
+  home.file.wallpaper = {
+    source = ./. + "/wallpaper.jpg";
+    target = "wallpaper.jpg";
+  };
 
-    # Cursor
-    xsession.pointerCursor = {
-        package = pkgs.numix-cursor-theme;
-        name = "Numix-Cursor-Light";
-        size = 32;
-    };
+  # Cursor
+  xsession.pointerCursor = {
+    package = pkgs.numix-cursor-theme;
+    name = "Numix-Cursor-Light";
+    size = 32;
+  };
 
-    # MIME set ups
-    xdg.enable = true;
-    xdg.mimeApps.enable = true;
-    xdg.mimeApps.defaultApplications = {
-        "x-scheme-handler/http" = [ "firefox.desktop" ];
-        "x-scheme-handler/https" = [ "firefox.desktop" ];
-        "x-scheme-handler/ftp" = [ "firefox.desktop" ];
-        "x-scheme-handler/ftps" = [ "firefox.desktop" ];
-        "x-scheme-handler/mailspring" = [ "Mailspring.desktop" ];
-    };
+  # MIME set ups
+  xdg.enable = true;
+  xdg.mimeApps.enable = true;
+  xdg.mimeApps.defaultApplications = {
+    "x-scheme-handler/http" = [ "firefox.desktop" ];
+    "x-scheme-handler/https" = [ "firefox.desktop" ];
+    "x-scheme-handler/ftp" = [ "firefox.desktop" ];
+    "x-scheme-handler/ftps" = [ "firefox.desktop" ];
+    "x-scheme-handler/mailspring" = [ "Mailspring.desktop" ];
+  };
 
-    # Mimic the clipboard stuff in MacOS
-    home.packages = [
-      (pkgs.writeShellScriptBin "pbcopy" ''
-        exec ${pkgs.xsel}/bin/xsel -ib
-      '')
-      (pkgs.writeShellScriptBin "pbpaste" ''
-        exec ${pkgs.xsel}/bin/xsel -ob
-      '')
-    ];
+  # Mimic the clipboard stuff in MacOS
+  home.packages = [
+    (pkgs.writeShellScriptBin "pbcopy" ''
+      exec ${pkgs.xsel}/bin/xsel -ib
+    '')
+    (pkgs.writeShellScriptBin "pbpaste" ''
+      exec ${pkgs.xsel}/bin/xsel -ob
+    '')
+  ];
+
+  # IBus configuration
+  dconf.settings."desktop/ibus/general" = {
+    engines-order = hm.gvariant.mkArray hm.gvariant.type.string [ "xkb:jp::jpn" "mozc-jp" "Bamboo" ];
+    reload-engines = hm.gvariant.mkArray hm.gvariant.type.string [ "xkb:jp::jpn" "mozc-jp" "Bamboo" ];
+  };
+  dconf.settings."desktop/ibus/general/hotkey" = {
+    triggers = hm.gvariant.mkArray hm.gvariant.type.string [ "<Meta>z" ];
+  };
 }
