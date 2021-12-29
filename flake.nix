ddc4b7fb8bdb438468eb1c74444177c1570bfff7
Natsu Kagami <nki@nkagami.me>
2021-12-29 13:04:48 -0500

    Change nixpkgs channel from `21.11` to `nixos-21.11`
    Note that `21.11` is a tag (i.e. it never changes), while
    `nixos-21.11` is a branch, so it gets updated over time.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "nki's systems";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/21.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs-unstable";
