1df631061a547c8e4517402905893ee118b78d64
Natsu Kagami <nki@nkagami.me>
2022-01-12 18:17:05 -0500

    More stuff for kakoune

--- home/kakoune/kaktex.nix
+++ home/kakoune/kaktex.nix
@@ -14,9 +14,11 @@ in
   # Source kaktex whenever we have a tex file
   programs.my-kakoune.rc = ''
     hook global WinSetOption filetype=(tex|latex) %{
+      hook window WinDisplay '.*' %{
         eval %sh{
           ${kaktexScript} set $kak_client $kak_session
         }
+      }
     }
   '';
 }
