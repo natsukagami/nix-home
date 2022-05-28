edbadbe622f82a7520854a6342b7a5d67dbaf859
Natsu Kagami <nki@nkagami.me>
2022-05-28 12:32:45 -0400

    Fix discord font
    Discord is not picking up fonts that aren't explicitly listed in the
    fontconfig list, so we add Japanese and Korean fonts into our mix.

--- modules/personal/fonts/default.nix
+++ modules/personal/fonts/default.nix
@@ -17,8 +17,8 @@ with lib;
     fontconfig = {
       defaultFonts = {
         emoji = lib.mkBefore [ "Blobmoji" ];
-        serif = lib.mkBefore [ "IBM Plex Serif" ];
-        sansSerif = lib.mkBefore [ "IBM Plex Sans" ];
+        serif = lib.mkBefore [ "IBM Plex Serif" "IBM Plex Sans JP" "IBM Plex Sans KR" ];
+        sansSerif = lib.mkBefore [ "IBM Plex Sans" "IBM Plex Sans JP" "IBM Plex Sans KR" ];
         monospace = lib.mkBefore [ "IBM Plex Mono" ];
       };
     };
