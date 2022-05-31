dec1c45a034e7c67e13899ec0d53569d638edaeb
Natsu Kagami <nki@nkagami.me>
2022-05-31 12:07:59 -0400

    Update to final 22.05 branch, change openrazer overlay
    ... to something newer, as our PR is in.

--- overlays/openrazer/default.nix
+++ overlays/openrazer/default.nix
@@ -1,29 +1,31 @@
 final: prev:
 let
+  version = "3.3.0";
   src = final.fetchFromGitHub {
-    owner = "natsukagami";
+    owner = "openrazer";
     repo = "openrazer";
-    rev = "28cd304e0389d26ded2a274b0f9ecd2debf057a0";
-    sha256 = "sha256-8vCR8/eZq0z0+K1vajjt9wDcV/2QwX9JJh9usWAEvBg=";
+    rev = "v${version}";
+    sha256 = "sha256-lElE1nIiJ5fk2DupHu43tmxRjRsS5xeL1Yz/LuRlgtM=";
   };
-in rec
+in
+rec
 {
   openrazer-daemon = prev.openrazer-daemon.overrideAttrs (old: {
-    inherit src;
+    inherit src version;
   });
 
   python3 = prev.python3.override {
     packageOverrides = self: super: {
       openrazer-daemon = super.openrazer-daemon.overrideAttrs (old: {
-        inherit src;
+        inherit src version;
       });
     };
   };
   python3Packages = python3.pkgs;
 
-  linuxPackages = prev.linuxPackages.extend (self: super: {
+  linuxPackages_latest = prev.linuxPackages_latest.extend (self: super: {
     openrazer = super.openrazer.overrideAttrs (old: {
-      inherit src;
+      inherit src version;
     });
   });
 }
