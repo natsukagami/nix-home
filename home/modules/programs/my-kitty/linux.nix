d885b3de4b646bdd9b490ad95c6f1e5ed31d5b8f
Natsu Kagami <nki@nkagami.me>
2022-10-12 18:05:42 +0200

    Add kitty shortcut for creating windows with cwd

--- home/modules/programs/my-kitty/linux.nix
+++ home/modules/programs/my-kitty/linux.nix
@@ -7,5 +7,10 @@ with lib;
   programs.kitty = mkIf (cfg.enable && pkgs.stdenv.isLinux) {
     # set the shell
     settings.shell = "${config.programs.fish.package}/bin/fish";
+
+    keybindings = {
+      "ctrl+shift+n" = "new_os_window_with_cwd";
+      "ctrl+shift+enter" = "new_window_with_cwd";
+    };
   };
 }
