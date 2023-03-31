e1b60f614582cabd0520a963bf3f8d7f41653ecb
Natsu Kagami <nki@nkagami.me>
2023-03-31 16:48:43 +0200

    Update blobmoji to 15

--- modules/personal/fonts/default.nix
+++ modules/personal/fonts/default.nix
@@ -1,6 +1,24 @@
 { pkgs, lib, config, ... }:
 
 with lib;
+let
+  noto-fonts-emoji-blob-bin =
+    let
+      pname = "noto-fonts-emoji-blob-bin";
+      version = "15.0";
+    in
+    pkgs.fetchurl {
+      name = "${pname}-${version}";
+      url = "https://github.com/C1710/blobmoji/releases/download/v${version}/Blobmoji.ttf";
+      sha256 = "sha256-n5yVk2w9x7UVrMe0Ho6nwu1Z9E/ktjo1UHdHKStoJWc=";
+
+      downloadToTemp = true;
+      recursiveHash = true;
+      postFetch = ''
+        install -Dm 444 $downloadedFile $out/share/fonts/blobmoji/Blobmoji.ttf
+      '';
+    };
+in
 {
   # Fonts
   config.fonts = {
@@ -27,3 +45,4 @@ with lib;
     fontDir.enable = true;
   } else { });
 }
+
