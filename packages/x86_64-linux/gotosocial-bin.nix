3a2c2b428c3b3cea370f53c8772d278f97ceb89b
Natsu Kagami <nki@nkagami.me>
2023-05-17 15:13:15 +0200

    Update gts to 0.9.0-rc1

--- packages/x86_64-linux/gotosocial-bin.nix
+++ packages/x86_64-linux/gotosocial-bin.nix
@@ -4,11 +4,11 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "gotosocial-bin";
-  version = "0.8.1";
+  version = "0.9.0-rc1";
 
   src = builtins.fetchurl {
     url = "https://github.com/superseriousbusiness/gotosocial/releases/download/v${version}/gotosocial_${version}_linux_amd64.tar.gz";
-    sha256 = "sha256:0vfgz236s4zqcv4a8bylp5znina26nvckdk1vgxbqkdnip3mnirj";
+    sha256 = "1liz4avmqlnsb0ry5qzha5rsa5g81jd755ppl8yx8cpiyrs7lj8w";
   };
 
   nativeBuildInputs = [ autoPatchelfHook ];
