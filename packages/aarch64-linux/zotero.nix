deee24487104fe11680d2a26954a8b8466ee2ec3
Natsu Kagami <nki@nkagami.me>
2023-06-16 14:56:17 +0200

    Update Zotero to 6.0.26
    Also: Steal desktop files and icons from Zotero package

--- packages/aarch64-linux/zotero.nix
+++ packages/aarch64-linux/zotero.nix
@@ -2,8 +2,8 @@
 
 let
   zotero-tar = builtins.fetchurl {
-    url = "https://repo.archlinuxcn.org/aarch64/zotero-6.0.20-1-aarch64.pkg.tar.zst";
-    sha256 = "sha256:1fqvcbffqfrnmfz7rcmbngik37wz9dh11q9shrd9cwkq6zay9b6k";
+    url = "https://repo.archlinuxcn.org/aarch64/zotero-6.0.26-1-aarch64.pkg.tar.zst";
+    sha256 = "sha256:0hz9y67bbc9rc6sp8v5i6aa890qvbngpf6hxx2krxrsh3xxn83y2";
   };
 
   zotero-src = runCommandLocal "zotero-src" { } ''
@@ -16,4 +16,9 @@ buildFHSEnvChroot {
   name = "zotero";
   targetPkgs = pkgs: with pkgs; [ gtk3 dbus-glib libstartup_notification libpaper ] ++ (with pkgs.xorg; [ libX11 libXt ]);
   runScript = "env QT_SCALE_FACTOR=2 ${zotero-src}/usr/lib/zotero/zotero";
+
+  extraInstallCommands = ''
+    cp --no-preserve=mode,ownership -r ${zotero-src}/usr/share $out/share
+    sed -i "s#/usr/bin/zotero#zotero#g" $out/share/applications/zotero.desktop
+  '';
 }
