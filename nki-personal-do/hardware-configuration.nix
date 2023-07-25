8c98a1a7e9a08e89096cd7e7ca67621037c623b8
Natsu Kagami <nki@nkagami.me>
2023-07-25 12:22:50 +0200

    Raise swap to 4gb

--- nki-personal-do/hardware-configuration.nix
+++ nki-personal-do/hardware-configuration.nix
@@ -6,7 +6,7 @@
   boot.initrd.kernelModules = [ "nvme" ];
   fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
   # swap
-  swapDevices = [{ device = "/var/swapfile"; size = 2 * 1024; }];
+  swapDevices = [{ device = "/var/swapfile"; size = 4 * 1024; }];
   # volumes
   fileSystems.data = {
     device = "/dev/disk/by-id/scsi-0HC_Volume_31812942";
