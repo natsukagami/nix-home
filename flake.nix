97b39262e3db67905d4b1fdd6b1c8d4948d878ac
Natsu Kagami <nki@nkagami.me>
2023-09-11 11:05:18 +0200

    Remove iog cache

--- flake.nix
+++ flake.nix
@@ -78,10 +78,10 @@
         ];
         # Binary Cache for Haskell.nix  
         nix.settings.trusted-public-keys = [
-          "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+          # "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
         ];
         nix.settings.substituters = [
-          "https://cache.iog.io"
+          # "https://cache.iog.io"
         ];
       };
       nixpkgsAsRegistry = nixpkgsAsRegistry_ nixpkgs;
