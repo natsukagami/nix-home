74fe3965b27447c4460502098ccd8e89bdacf1fb
Natsu Kagami <nki@nkagami.me>
2023-09-12 07:43:33 +0200

    More tight restrictions for tlp

--- nki-yoga-g8/configuration.nix
+++ nki-yoga-g8/configuration.nix
@@ -28,6 +28,20 @@
   };
   services.tlp.enable = true;
   services.tlp.settings = {
+    CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+    CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+    CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+    CPU_MIN_PERF_ON_AC = 0;
+    CPU_MAX_PERF_ON_AC = 100;
+    CPU_MIN_PERF_ON_BAT = 0;
+    CPU_MAX_PERF_ON_BAT = 30;
+
+    PLATFORM_PROFILE_ON_AC = "balanced";
+    PLATFORM_PROFILE_ON_BAT = "low-power";
+
     USB_AUTOSUSPEND = 0;
   };
   services.power-profiles-daemon.enable = false;
