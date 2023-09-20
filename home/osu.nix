b300f430609247a7efbb923f69b90008db12fb89
Natsu Kagami <nki@nkagami.me>
2023-09-20 21:54:12 +0200

    Add back latest osu

--- home/osu.nix
+++ home/osu.nix
@@ -1,29 +1,31 @@
 { pkgs, config, lib, ... }:
 
 let
-  osu-pkg = with pkgs; with lib;
-    appimageTools.wrapType2 rec {
-      pname = "osu-lazer-bin";
-      version = "2023.123.0";
+  osu-pkg = pkgs.unstable.osu-lazer-bin;
+  # with pkgs; with lib;
+  #   appimageTools.wrapType2 rec {
+  #     pname = "osu-lazer-bin";
+  #     version = "2023.914.0";
 
-      src = fetchurl {
-        url = "https://github.com/ppy/osu/releases/download/${version}/osu.AppImage";
-        sha256 = "sha256-edu93pvTEM5/s0kW55U1xfYGDl0eUpGXypvuYIwsM3w=";
-      };
+  #     src = fetchurl {
+  #       url = "https://github.com/ppy/osu/releases/download/${version}/osu.AppImage";
+  #       # sha256 = "sha256-edu93pvTEM5/s0kW55U1xfYGDl0eUpGXypvuYIwsM3w=";
+  #       hash = lib.fakeHash;
+  #     };
 
-      extraPkgs = pkgs: with pkgs; [ icu ];
+  #     extraPkgs = pkgs: with pkgs; [ icu ];
 
-      extraInstallCommands =
-        let contents = appimageTools.extract { inherit pname version src; };
-        in
-        ''
-          mv -v $out/bin/${pname}-${version} $out/bin/osu\!
-          install -m 444 -D ${contents}/osu\!.desktop -t $out/share/applications
-          for i in 16 32 48 64 96 128 256 512 1024; do
-            install -D ${contents}/osu\!.png $out/share/icons/hicolor/''${i}x$i/apps/osu\!.png
-          done
-        '';
-    };
+  #     extraInstallCommands =
+  #       let contents = appimageTools.extract { inherit pname version src; };
+  #       in
+  #       ''
+  #         mv -v $out/bin/${pname}-${version} $out/bin/osu\!
+  #         install -m 444 -D ${contents}/osu\!.desktop -t $out/share/applications
+  #         for i in 16 32 48 64 96 128 256 512 1024; do
+  #           install -D ${contents}/osu\!.png $out/share/icons/hicolor/''${i}x$i/apps/osu\!.png
+  #         done
+  #       '';
+  #   };
 in
 {
   home.packages = [ osu-pkg ];
