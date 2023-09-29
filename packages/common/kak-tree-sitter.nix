08fef1878ffe9a1883fdfa4d58b4542bc86f7a82
Natsu Kagami <nki@nkagami.me>
2023-09-29 17:48:01 +0200

    Update kak-tree-sitter

--- packages/common/kak-tree-sitter.nix
+++ packages/common/kak-tree-sitter.nix
@@ -3,31 +3,18 @@ let
   src = fetchFromGitHub {
     owner = "phaazon";
     repo = "kak-tree-sitter";
-    rev = "facf55f77171ae0d33332c6d54b5492e544f9ca1";
-    hash = "sha256-XMHgP4SO2HT6NthQkKUwU1rOwVFXDp7FsM99zqM4Q04=";
+    rev = "3567f648bbf6a5d556c43bde5433dff45eabd693";
+    hash = "sha256-xr7CtOfMO4nRu2MOIQX3jR0wsKGsjYiF/TGXSAsidM4=";
   };
 
   kak-tree-sitter = rustPlatform.buildRustPackage rec {
     inherit src;
     pname = "kak-tree-sitter";
-    version = "0.4.3";
-    cargoHash = "sha256-JvMcwdllq0dacceZsI14cCnV7aW7wmU3h/Y9SAwHVtM=";
-    cargoBuildOptions = [ "--package" "kak-tree-sitter" ];
-  };
+    version = "0.4.6";
+    cargoHash = "sha256-6HJxJTr4P1/6Yy3/dtfiaCFoHA4iKvmuwg51jTYU2eo=";
+    cargoBuildOptions = [ "--package" "kak-tree-sitter" "--package" "ktsctl" ];
 
-  ktsctl = rustPlatform.buildRustPackage rec {
-    inherit src;
-    name = "ktsctl";
-    version = "0.3.1";
-    cargoHash = "sha256-pyCUiekj79euOtS43mu9Fti+HZizaV069068h61uOT8=";
-
-    cargoBuildOptions = [ "--package" "ktsctl" ];
-
-    buildInputs = [ clang git ];
+    nativeBuildInputs = [ clang git ];
   };
 in
-symlinkJoin {
-  name = "kak-tree-sitter";
-  paths = [ kak-tree-sitter ktsctl ];
-}
-
+kak-tree-sitter
