67cafa589278b011ce6a8ad1739d55fc3feefe07
Natsu Kagami <nki@nkagami.me>
2023-10-10 23:29:08 +0200

    Add cachix of me

---
+++ cachix/natsukagami.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://natsukagami.cachix.org"
+      ];
+      trusted-public-keys = [
+        "natsukagami.cachix.org-1:3U6GV8i8gWEaXRUuXd2S4ASfYgdl2QFPWg4BKPbmYiQ="
+      ];
+    };
+  };
+}
