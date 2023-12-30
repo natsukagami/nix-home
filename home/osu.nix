8f82978e8d640ac4b2aafdcef7ab65b19667e76a
Natsu Kagami <nki@nkagami.me>
2023-12-30 19:56:01 +0100

    Update osu-lazer

--- home/osu.nix
+++ home/osu.nix
@@ -1,36 +1,36 @@
 { pkgs, config, lib, ... }:
 
 let
-  osu-pkg =
-    # pkgs.unstable.osu-lazer-bin.overrideAttrs (attrs: rec {
-    #   version = "2023.1130.0";
-    #   src = pkgs.fetchurl {
-    #     url = "https://github.com/ppy/osu/releases/download/${version}/osu.AppImage";
-    #     hash = "sha256-dQuyKjCZaIl3uaI81qRMt5NzBxfmyROVbJrVAqzuZxg=";
-    #   };
-    # });
-    with pkgs; with lib;
-    appimageTools.wrapType2 rec {
-      pname = "osu-lazer-bin";
-      version = "2023.1130.0";
-      src = pkgs.fetchurl {
-        url = "https://github.com/ppy/osu/releases/download/${version}/osu.AppImage";
-        hash = "sha256-dQuyKjCZaIl3uaI81qRMt5NzBxfmyROVbJrVAqzuZxg=";
-      };
+  osu-pkg = pkgs.unstable.osu-lazer-bin;
+  # pkgs.unstable.osu-lazer-bin.overrideAttrs (attrs: rec {
+  #   version = "2023.1130.0";
+  #   src = pkgs.fetchurl {
+  #     url = "https://github.com/ppy/osu/releases/download/${version}/osu.AppImage";
+  #     hash = "sha256-dQuyKjCZaIl3uaI81qRMt5NzBxfmyROVbJrVAqzuZxg=";
+  #   };
+  # });
+  # with pkgs; with lib;
+  # appimageTools.wrapType2 rec {
+  #   pname = "osu-lazer-bin";
+  #   version = "2023.1130.0";
+  #   src = pkgs.fetchurl {
+  #     url = "https://github.com/ppy/osu/releases/download/${version}/osu.AppImage";
+  #     hash = "sha256-dQuyKjCZaIl3uaI81qRMt5NzBxfmyROVbJrVAqzuZxg=";
+  #   };
 
-      extraPkgs = pkgs: with pkgs; [ icu ];
+  #   extraPkgs = pkgs: with pkgs; [ icu ];
 
-      extraInstallCommands =
-        let contents = appimageTools.extract { inherit pname version src; };
-        in
-        ''
-          mv -v $out/bin/${pname}-${version} $out/bin/osu\!
-          install -m 444 -D ${contents}/osu\!.desktop -t $out/share/applications
-          for i in 16 32 48 64 96 128 256 512 1024; do
-            install -D ${contents}/osu\!.png $out/share/icons/hicolor/''${i}x$i/apps/osu\!.png
-          done
-        '';
-    };
+  #   extraInstallCommands =
+  #     let contents = appimageTools.extract { inherit pname version src; };
+  #     in
+  #     ''
+  #       mv -v $out/bin/${pname}-${version} $out/bin/osu\!
+  #       install -m 444 -D ${contents}/osu\!.desktop -t $out/share/applications
+  #       for i in 16 32 48 64 96 128 256 512 1024; do
+  #         install -D ${contents}/osu\!.png $out/share/icons/hicolor/''${i}x$i/apps/osu\!.png
+  #       done
+  #     '';
+  # };
 in
 {
   home.packages = [ osu-pkg ];
