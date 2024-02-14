efdad6975d8a1c9e58c3078316f18a270bf557b5
Natsu Kagami <nki@nkagami.me>
2024-02-14 01:47:23 +0100

    Temporarily deploy youmu on rosu-v2 branch

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
 
     # ---
     # Imported apps
-    youmubot.url = "github:natsukagami/youmubot";
+    youmubot.url = "github:natsukagami/youmubot/rosu-v2";
     swayfx = {
       url = github:WillPower3309/swayfx;
       inputs.nixpkgs.follows = "nixpkgs";
