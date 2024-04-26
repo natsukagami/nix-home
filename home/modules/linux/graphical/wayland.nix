e17341a42f627108bf8fc6fa4f7a56fb416acf87
Natsu Kagami <nki@nkagami.me>
2024-04-26 13:16:10 -0400

    Update pbcopy/paste

--- home/modules/linux/graphical/wayland.nix
+++ home/modules/linux/graphical/wayland.nix
@@ -68,10 +68,10 @@ with lib;
 
       # Mimic the clipboard stuff in MacOS
       (pkgs.writeShellScriptBin "pbcopy" ''
-        exec ${pkgs.wl-clipboard}/bin/wl-copy
+        exec ${pkgs.wl-clipboard}/bin/wl-copy "$@"
       '')
       (pkgs.writeShellScriptBin "pbpaste" ''
-        exec ${pkgs.wl-clipboard}/bin/wl-paste -n
+        exec ${pkgs.wl-clipboard}/bin/wl-paste "$@"
       '')
     ];
 
