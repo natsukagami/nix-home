eed4c251274009aacddcb68efd55dbc5041f4f00
Natsu Kagami <nki@nkagami.me>
2024-06-01 17:29:45 +0200

    Update nixpkgs to final release

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "nki's systems";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     darwin.url = "github:lnl7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs-unstable";
