d0943fd8b48843459bce9f1ab90bdb4f2ff137f0
Natsu Kagami <nki@nkagami.me>
2024-06-09 18:44:23 +0200

    Add Blobmoji fallback to default fonts

--- modules/personal/fonts/default.nix
+++ modules/personal/fonts/default.nix
@@ -1,24 +1,6 @@
-{ pkgs, lib, config, ... }:
+{ pkgs, lib, ... }:
 
 with lib;
-let
-  noto-fonts-emoji-blob-bin =
-    let
-      pname = "noto-fonts-emoji-blob-bin";
-      version = "15.0";
-    in
-    pkgs.fetchurl {
-      name = "${pname}-${version}";
-      url = "https://github.com/C1710/blobmoji/releases/download/v${version}/Blobmoji.ttf";
-      sha256 = "sha256-n5yVk2w9x7UVrMe0Ho6nwu1Z9E/ktjo1UHdHKStoJWc=";
-
-      downloadToTemp = true;
-      recursiveHash = true;
-      postFetch = ''
-        install -Dm 444 $downloadedFile $out/share/fonts/blobmoji/Blobmoji.ttf
-      '';
-    };
-in
 {
   imports = [ ./mounting.nix ];
   # Fonts
@@ -37,9 +19,9 @@ in
     fontconfig = {
       defaultFonts = {
         emoji = lib.mkBefore [ "Blobmoji" ];
-        serif = lib.mkBefore [ "IBM Plex Serif" "IBM Plex Sans JP" "IBM Plex Sans KR" ];
-        sansSerif = lib.mkBefore [ "IBM Plex Sans" "IBM Plex Sans JP" "IBM Plex Sans KR" ];
-        monospace = lib.mkBefore [ "IBM Plex Mono" ];
+        serif = lib.mkBefore [ "IBM Plex Serif" "IBM Plex Sans JP" "IBM Plex Sans KR" "Blobmoji" ];
+        sansSerif = lib.mkBefore [ "IBM Plex Sans" "IBM Plex Sans JP" "IBM Plex Sans KR" "Blobmoji" ];
+        monospace = lib.mkBefore [ "IBM Plex Mono" "IBM Plex Sans JP" "Blobmoji" ];
       };
       localConf = ''
         <?xml version="1.0"?>
@@ -51,6 +33,7 @@ in
               <family>IBM Plex Sans</family>
               <family>IBM Plex Sans JP</family>
               <family>IBM Plex Sans KR</family>
+              <family>Blobmoji</family>
             </prefer>
           </alias>
         </fontconfig>
