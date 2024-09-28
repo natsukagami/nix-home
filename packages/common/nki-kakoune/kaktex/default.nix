d160c48ba20cdbb5509095e865421664d11bf2b7
Natsu Kagami <nki@nkagami.me>
2024-09-28 16:34:57 +0200

    kakoune: add back kaktex

---
+++ packages/common/nki-kakoune/kaktex/default.nix
@@ -0,0 +1,22 @@
+{ fish, lib, writeScript, writeTextDir, kakouneUtils, ... }:
+let
+  kaktex-script = writeScript "kaktex" ''
+    #!/usr/bin/env ${lib.getExe fish}
+
+    ${builtins.readFile ./kaktex.fish}
+  '';
+  kaktex = writeTextDir "kaktex.kak" ''
+    hook global WinSetOption filetype=(tex|latex) %{
+      hook window WinDisplay '.*' %{
+        eval %sh{
+          ${kaktex-script} set $kak_client $kak_session
+        }
+      }
+    }
+  '';
+in
+kakouneUtils.buildKakounePluginFrom2Nix {
+  pname = "kaktex";
+  version = "latest";
+  src = kaktex;
+}
