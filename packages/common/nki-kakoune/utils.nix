53cafeb0210aeb3baef41fe81449a774363d9d13
Natsu Kagami <nki@nkagami.me>
2024-10-07 16:31:33 +0200

    Add tree-sitter

--- packages/common/nki-kakoune/utils.nix
+++ packages/common/nki-kakoune/utils.nix
@@ -1,5 +1,7 @@
-{ lib, writeFile, ... }: {
-  mkFacesScript = name: faces: writeFile "${name}-faces.kak" (
-    lib.concatStringsSep "\n" (builtins.attrValues (builtins.mapAttrs (name: face: "face global ${name} \"${face}\"") faces))
-  );
+{ lib, writeTextDir, ... }: {
+  mkFacesScript = name: faces: writeTextDir "share/kak/autoload/${name}/faces.kak" ''
+    hook global KakBegin .* {
+    ${lib.concatStringsSep "\n" (builtins.attrValues (builtins.mapAttrs (name: face: "  face global ${name} \"${face}\"") faces))}
+    }
+  '';
 }
