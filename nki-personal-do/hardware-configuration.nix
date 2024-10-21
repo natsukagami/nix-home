4a5fbf3d9ce64f80f38b95a84f11019761437e01
Natsu Kagami <nki@nkagami.me>
2024-10-21 05:04:00 +0200

    Move stuff to a new drive

--- nki-personal-do/hardware-configuration.nix
+++ nki-personal-do/hardware-configuration.nix
@@ -9,9 +9,11 @@
   swapDevices = [{ device = "/var/swapfile"; size = 4 * 1024; priority = 1024; }];
   zramSwap.enable = true;
   # volumes
-  fileSystems.data = {
-    device = "/dev/disk/by-id/scsi-0HC_Volume_31812942";
-    fsType = "ext4";
+  services.btrfs.autoScrub.enable = true;
+  fileSystems.nki-data = {
+    device = "/dev/disk/by-id/scsi-0HC_Volume_101470796";
+    fsType = "btrfs";
     mountPoint = "/mnt/data";
+    options = [ "compress=zstd" ];
   };
 }
