9c5f0981a3e88328d84a63c86a8fc0e5df953ad6
Natsu Kagami <nki@nkagami.me>
2024-12-15 23:51:33 +0100

    Update osu

--- home/osu.nix
+++ home/osu.nix
@@ -5,10 +5,10 @@ let
   osu-pkg = with pkgs; with lib;
     appimageTools.wrapType2 rec {
       pname = "osu-lazer-bin";
-      version = "2024.1115.3";
+      version = "2024.1208.0";
       src = fetchurl {
         url = "https://github.com/ppy/osu/releases/download/${version}/osu.AppImage";
-        sha256 = "sha256-kwZHy0FfOUFIWvyOj0ghlQz05U+Lnzl5TgC4T6bhm7o=";
+        hash = "sha256-gRUr7jf0+Xbfz8FurPk/o7F67TYisdNySNzVWEMb1es=";
       };
       extraPkgs = pkgs: with pkgs; [ icu ];
 
@@ -19,7 +19,7 @@ let
           mv -v $out/bin/${pname} $out/bin/osu\!
           install -m 444 -D ${contents}/osu\!.desktop -t $out/share/applications
           for i in 16 32 48 64 96 128 256 512 1024; do
-            install -D ${contents}/osu\!.png $out/share/icons/hicolor/''${i}x$i/apps/osu\!.png
+            install -D ${contents}/osu.png $out/share/icons/hicolor/''${i}x$i/apps/osu.png
           done
         '';
     };
