bfaa5c00c5a358887ca26060430f9073f2564245
Natsu Kagami <nki@nkagami.me>
2024-12-20 12:32:32 +0100

    yoga: rename obsolete libinput path

--- nki-yoga-g8/configuration.nix
+++ nki-yoga-g8/configuration.nix
@@ -80,7 +80,7 @@ in
   services.printing.drivers = with pkgs; [ epfl-cups-drivers ];
 
   # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
+  services.libinput.enable = true;
   # Keyboard
   services.input-remapper.enable = true;
   services.input-remapper.serviceWantedBy = [ "multi-user.target" ];
