983a1fb0714631d8f556e7bb8a581760121c5cb8
Natsu Kagami <nki@nkagami.me>
2024-12-20 20:53:30 +0100

    Update osu

--- home/osu.nix
+++ home/osu.nix
@@ -5,7 +5,7 @@ let
   osu-pkg = with pkgs; with lib;
     appimageTools.wrapType2 rec {
       pname = "osu-lazer-bin";
-      version = "2024.1208.0";
+      version = "2024.1219.2";
       src = fetchurl {
         url = "https://github.com/ppy/osu/releases/download/${version}/osu.AppImage";
         hash = "sha256-gRUr7jf0+Xbfz8FurPk/o7F67TYisdNySNzVWEMb1es=";
