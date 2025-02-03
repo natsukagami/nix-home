30c48b9340b1df5237601085356fb51a4d05bfae
Natsu Kagami <nki@nkagami.me>
2025-02-03 15:41:30 +0100

    Update lanzaboote

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
 
     # --- Secure boot
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.1";
+      url = "github:nix-community/lanzaboote/v0.4.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
