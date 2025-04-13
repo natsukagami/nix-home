578ae91c59330ac0a46eb5289f17d2af2343bb87
Natsu Kagami <nki@nkagami.me>
2025-04-14 00:40:29 +0200

    Automatically move gotosocial to later version if already in nixpkgs

--- overlay.nix
+++ overlay.nix
@@ -31,31 +31,38 @@ let
   };
 
   overlay-versioning = final: prev: {
-    gotosocial = prev.gotosocial.overrideAttrs (attrs: rec {
-      version = "0.18.1";
-      ldflags = [
-        "-s"
-        "-w"
-        "-X main.Version=${version}"
-      ];
-      doCheck = false;
+    gotosocial =
+      let
+        local = prev.gotosocial.overrideAttrs (attrs: rec {
+          version = "0.18.1";
+          ldflags = [
+            "-s"
+            "-w"
+            "-X main.Version=${version}"
+          ];
+          doCheck = false;
 
-      web-assets = final.fetchurl {
-        url = "https://github.com/superseriousbusiness/gotosocial/releases/download/v${version}/gotosocial_${version}_web-assets.tar.gz";
-        hash = "sha256-5MSABLPyTbFMTno9vUDvLT9h7oQM6eNUuwD+dsHiCLo=";
-      };
-      src = final.fetchFromGitHub {
-        owner = "superseriousbusiness";
-        repo = "gotosocial";
-        rev = "v${version}";
-        hash = "sha256-4jV1G1HwpIST2Y27RAhJB3CoJevwuhxdzi615hj0Qv0=";
-      };
-      postInstall = ''
-        tar xf ${web-assets}
-        mkdir -p $out/share/gotosocial
-        mv web $out/share/gotosocial/
-      '';
-    });
+          web-assets = final.fetchurl {
+            url = "https://github.com/superseriousbusiness/gotosocial/releases/download/v${version}/gotosocial_${version}_web-assets.tar.gz";
+            hash = "sha256-5MSABLPyTbFMTno9vUDvLT9h7oQM6eNUuwD+dsHiCLo=";
+          };
+          src = final.fetchFromGitHub {
+            owner = "superseriousbusiness";
+            repo = "gotosocial";
+            rev = "v${version}";
+            hash = "sha256-4jV1G1HwpIST2Y27RAhJB3CoJevwuhxdzi615hj0Qv0=";
+          };
+          postInstall = ''
+            tar xf ${web-assets}
+            mkdir -p $out/share/gotosocial
+            mv web $out/share/gotosocial/
+          '';
+        });
+      in
+      if builtins.compareVersions prev.gotosocial.version local.version == -1 then
+        local
+      else
+        prev.gotosocial;
 
     input-remapper = final.unstable.input-remapper;
 
