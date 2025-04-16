ea253a957d1614000d51341b1f705aad9349cb04
Natsu Kagami <nki@nkagami.me>
2025-04-16 13:39:16 +0200

    Update openconnect-epfl: no longer needs to specify method

--- home/modules/programs/openconnect-epfl.nix
+++ home/modules/programs/openconnect-epfl.nix
@@ -12,12 +12,11 @@ let
       rbw
     ];
     text = ''
-      METHOD="Microsoft Entra ID"
       RBW_ENTRY="EPFL Microsoft Auth"
       GASPAR_PASSWORD=$(rbw get "$RBW_ENTRY")
       GASPAR_TOKEN=$(rbw code "$RBW_ENTRY")
 
-      printf "\n%s\n%s\n%s\n" "$METHOD" "$GASPAR_PASSWORD" "$GASPAR_TOKEN" | command sudo openconnect \
+      printf "%s\n" "$GASPAR_PASSWORD" "$GASPAR_TOKEN" | command sudo openconnect \
           --passwd-on-stdin \
           -u "pham" \
           --useragent='AnyConnect' \
