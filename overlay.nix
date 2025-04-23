4f1e681068b3559c2cb882ed5a6a99688acf859d
Natsu Kagami <nki@nkagami.me>
2025-04-23 16:36:33 -0400

    Override rofi-wayland to allow keybindings involving alt

--- overlay.nix
+++ overlay.nix
@@ -124,6 +124,19 @@ let
 
         propagatedBuildInputs = (prev.propagatedBuildInputs or [ ]) ++ [ final.ncurses ];
       });
+
+    rofi-wayland-unwrapped =
+      assert final.lib.assertMsg (prev.rofi-wayland-unwrapped.version == "1.7.8+wayland1")
+        "We only need this for https://github.com/lbonn/rofi/commit/f2f22e7edc635f7e4022afcf81a411776268c1c3. Use upstream package instead";
+      prev.rofi-wayland-unwrapped.overrideAttrs (prev: {
+        src = final.fetchFromGitHub {
+          owner = "lbonn";
+          repo = "rofi";
+          rev = "3bec3fac59394a475d162e72d5be2fb042115274";
+          fetchSubmodules = true;
+          hash = "sha256-xkf5HWXvzanT9tCDHbVpgUAmQlqmrPMlnv6MbcN0k9E=";
+        };
+      });
   };
 
   overlay-libs = final: prev: {
