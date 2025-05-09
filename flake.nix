da4576b1d2445ba45c9f70c9723efeaed4ce3016
Natsu Kagami <nki@nkagami.me>
2025-05-09 23:37:24 +0200

    Update matrix-conduit

--- flake.nix
+++ flake.nix
@@ -43,7 +43,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     dtth-phanpy.url = "git+ssh://gitea@git.dtth.ch/nki-dtth/phanpy?ref=dtth-fork";
-    conduit.url = "gitlab:famedly/conduit/v0.9.0";
+    conduit.url = "gitlab:famedly/conduit/v0.10.1";
     nix-gaming.url = "github:fufexan/nix-gaming";
     zen-browser.url = "github:youwen5/zen-browser-flake";
     niri.url = "github:sodiboo/niri-flake";
