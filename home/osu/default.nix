e70421ff67f8487a0681a83d089ac810fb84d92f
Natsu Kagami <nki@nkagami.me>
2025-06-16 23:58:44 +0200

    Update osu to latest tachyon

--- home/osu/default.nix
+++ home/osu/default.nix
@@ -7,10 +7,10 @@ let
     with lib;
     appimageTools.wrapType2 rec {
       pname = "osu-lazer-bin";
-      version = "2025.607.0";
+      version = "2025.616.0-tachyon";
       src = fetchurl {
         url = "https://github.com/ppy/osu/releases/download/${version}/osu.AppImage";
-        hash = "sha256-jG3KedllnVNd5TLSkKYae2V8CzN90g5lJhT4EKI+nuk=";
+        hash = "sha256-K8jyqqs4UBZTZ0Qzp9X+94j7b/HvbzEkAwk/2MR1aNc=";
       };
       extraPkgs = pkgs: with pkgs; [ icu ];
 
