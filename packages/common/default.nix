b30e2640c7d68f38dc17c31a27b7421f2dd8838d
Natsu Kagami <nki@nkagami.me>
2025-08-29 17:45:21 +0200

    Use upstream ttaenc

--- packages/common/default.nix
+++ packages/common/default.nix
@@ -1,6 +1,5 @@
 final: prev: {
   epfl-cups-drivers = final.callPackage ./epfl-cups-drivers { };
-  ttaenc = final.callPackage ./ttaenc.nix { };
   suwako-cursors = final.callPackage ./suwako-cursors { };
   nki-kakoune = final.callPackage ./nki-kakoune { };
   gotosocial-dtth = final.callPackage ./gotosocial { };
