2e72bdc0652846f4e94a5c20f0d8646a8d1b334a
Natsu Kagami <nki@nkagami.me>
2025-09-08 10:41:12 +0200

    Remove custom niri.nix

--- packages/common/default.nix
+++ packages/common/default.nix
@@ -3,5 +3,4 @@ final: prev: {
   suwako-cursors = final.callPackage ./suwako-cursors { };
   nki-kakoune = final.callPackage ./nki-kakoune { };
   gotosocial-dtth = final.callPackage ./gotosocial { };
-  niri = final.callPackage ./niri.nix { };
 }
