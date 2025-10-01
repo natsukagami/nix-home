705f9831aa0a600f03f7d10d07284a48a053dbe4
Natsu Kagami <nki@nkagami.me>
2025-10-01 22:38:49 +0200

    Don't cause an environment variable clash with kakoune-lsp

--- home/modules/programs/my-kakoune/fish-session.nix
+++ home/modules/programs/my-kakoune/fish-session.nix
@@ -14,34 +14,34 @@ in
   config = mkIf cfg.enable-fish-session {
     programs.fish.functions = {
       kak-session = ''
-        if set -q kak_session
-          echo "Another kakoune session ($kak_session) is currently running. Do `kill-kak-session` to stop it." >&2
+        if set -q fish_kak_session
+          echo "Another kakoune session ($fish_kak_session) is currently running. Do `kill-kak-session` to stop it." >&2
           return 1
         end
 
         if test (count $argv) -ge 1
-          set -gx kak_session $argv[1]
+          set -gx fish_kak_session $argv[1]
         else
-          set -gx kak_session "kak-"(random)
+          set -gx fish_kak_session "kak-"(random)
         end
 
         # Start a new kakoune session
-        kak -s $kak_session -d &
-        echo "New kakoune session started (pid = $last_pid, session name = $kak_session)."
+        kak -s $fish_kak_session -d &
+        echo "New kakoune session started (pid = $last_pid, session name = $fish_kak_session)."
 
         # Rebind $VISUAL, $EDITOR and e command
-        set -gx VISUAL kak -c $kak_session
-        set -gx EDITOR kak -c $kak_session
+        set -gx VISUAL kak -c $fish_kak_session
+        set -gx EDITOR kak -c $fish_kak_session
       '';
 
       kill-kak-session = ''
-        if not set -q kak_session
+        if not set -q fish_kak_session
           echo "No kakoune session found on this terminal instance." >&2
           return 1
         end
 
-        echo kill | kak -p $kak_session
-        set -eg kak_session
+        echo kill | kak -p $fish_kak_session
+        set -eg fish_kak_session
 
         # Rebind $VISUAL, $EDITOR and e command
         set -gx VISUAL "kak"
@@ -50,10 +50,10 @@ in
     };
     programs.fish.tide = {
       items.kakoune = ''
-        if set -q kak_session
+        if set -q fish_kak_session
           set -U tide_kakoune_color FFA500
           set -U tide_kakoune_bg_color normal
-          _tide_print_item kakoune " " "e[$kak_session]"
+          _tide_print_item kakoune " " "e[$fish_kak_session]"
         end
       '';
       rightItems = mkAfter [ "kakoune" ];
