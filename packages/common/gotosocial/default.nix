982ceb976250c8321f882a6dbe3d26c878fb0c0c
Natsu Kagami <nki@nkagami.me>
2025-12-13 20:16:40 +0100

    Update gotosocial to 0.20.2

--- packages/common/gotosocial/default.nix
+++ packages/common/gotosocial/default.nix
@@ -7,7 +7,7 @@
 gotosocial.overrideAttrs (
   finalAttrs: prevAttrs: {
     pname = "gotosocial-dtth";
-    version = "0.20.1";
+    version = "0.20.2";
     ldflags = [
       "-s"
       "-w"
@@ -16,11 +16,11 @@ gotosocial.overrideAttrs (
     doCheck = false;
     web-assets = fetchurl {
       url = "https://codeberg.org/superseriousbusiness/gotosocial/releases/download/v${finalAttrs.version}/gotosocial_${finalAttrs.version}_web-assets.tar.gz";
-      hash = "sha256-0WvaPUVTMYd1tz7Rtmlp37vx/co4efhDdSWBc4gUzAU=";
+      hash = "sha256-85tFn3LsuMbLoiH6FFtBK60GhclfTsSiI7K/iNLadjY=";
     };
     src = fetchzip {
       url = "https://codeberg.org/superseriousbusiness/gotosocial/releases/download/v${finalAttrs.version}/gotosocial-${finalAttrs.version}-source-code.tar.gz";
-      hash = "sha256-8z2tBiEVcof0/G41gpc0S8Dye5nynwHSJpTzo/ZseFs=";
+      hash = "sha256-H5+1BZ1jIISU6EPszIuOhqowoMe9WF36BGwV7TpAqj8=";
       stripRoot = false;
     };
     postInstall = ''
