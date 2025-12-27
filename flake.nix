33ba7d2751ab0f06bc2bd3326dae61c3977acddf
Natsu Kagami <nki@nkagami.me>
2025-12-27 22:13:07 +0100

    Update conduit to 0.10.10

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     dtth-phanpy.url = "git+ssh://gitea@git.dtth.ch/nki-dtth/phanpy?ref=dtth-fork";
-    conduit.url = "gitlab:famedly/conduit/v0.10.9";
+    conduit.url = "gitlab:famedly/conduit/v0.10.10";
     nix-gaming.url = "github:fufexan/nix-gaming";
     zen-browser.url = "github:youwen5/zen-browser-flake";
     niri-stable.url = "github:YaLTeR/niri/v25.11";
