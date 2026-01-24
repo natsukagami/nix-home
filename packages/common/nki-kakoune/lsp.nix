c559913a65accacade86a0171e09b3ea8f144537
Natsu Kagami <nki@nkagami.me>
2026-01-24 11:58:28 +0100

    Use nixfmt instead of nixfmt-rfc-style

--- packages/common/nki-kakoune/lsp.nix
+++ packages/common/nki-kakoune/lsp.nix
@@ -7,7 +7,7 @@
   ccls,
   gopls,
   nil,
-  nixfmt-rfc-style,
+  nixfmt,
   ltex-ls,
   nodePackages,
   tailwindcss-language-server,
@@ -117,7 +117,7 @@ let
                 ".git"
               ];
               settings.nil = {
-                formatting.command = [ "${lib.getExe nixfmt-rfc-style}" ];
+                formatting.command = [ "${lib.getExe nixfmt}" ];
               };
             };
             # pylsp = {
