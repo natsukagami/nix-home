bc93921ea4468befc963bd2acf349ddc3f20e604
Natsu Kagami <nki@nkagami.me>
2026-03-26 16:32:22 +0100

    Move nki-personal-do from zram to zswap

--- nki-personal-do/hardware-configuration.nix
+++ nki-personal-do/hardware-configuration.nix
@@ -21,7 +21,13 @@
       priority = 1024;
     }
   ];
-  zramSwap.enable = true;
+  # Zswap
+  boot.kernelParams = [
+    "zswap.enabled=1" # enables zswap
+    "zswap.compressor=zstd" # compression algorithm
+    "zswap.max_pool_percent=40" # maximum percentage of RAM that zswap is allowed to use
+    "zswap.shrinker_enabled=1" # whether to shrink the pool proactively on high memory pressure    ];
+  ];
   # volumes
   services.btrfs.autoScrub.enable = true;
   fileSystems.data = {
