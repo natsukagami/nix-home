96b2ce7a6e8df8993c9d148306f4d796e01a9c28
Natsu Kagami <nki@nkagami.me>
2026-08-28 17:26:26 +0200

    Override ddcci driver
    Details at nixOS/nixpkgs#554041

--- modules/common/linux/default.nix
+++ modules/common/linux/default.nix
@@ -284,7 +284,22 @@ in
   config = mkIf cfg.enable {
     ## Boot Configuration
     # Set kernel version to latest
-    boot.kernelPackages = mkDefault pkgs.linuxPackages_latest;
+    boot.kernelPackages = mkDefault (
+      pkgs.linuxPackages_latest.extend (
+        _final: prev: {
+          ddcci-driver = prev.ddcci-driver.overrideAttrs (oldAttrs: {
+            patches = [
+              (pkgs.fetchpatch {
+                name = "Use-sysfs_emit-and-field-width-specifier.patch";
+                url = "https://gitlab.com/liquidnya/ddcci-driver-linux/-/commit/9510aa4aebf32678884f55ae251e54012a354ed1.patch";
+                hash = "sha256-s12ers7nPFaHOB+8/S8t3dtdoR6slukkfNPdghgftNs=";
+              })
+            ]
+            ++ (oldAttrs.patches or [ ]);
+          });
+        }
+      )
+    );
     # Use the systemd-boot EFI boot loader.
     boot = {
       loader.timeout = 60;
